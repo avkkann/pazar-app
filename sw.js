c04f7d595301d4308856527d9ae99976a6f74883
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-05-19 02:52:42 +0300

    feat: profil ekrani + firsatlar kart duzeltme + SW v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v8';
+const CACHE_NAME = 'pazar-cache-v9';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
