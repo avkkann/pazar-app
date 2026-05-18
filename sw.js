e79c919bf1d3b0165ac0fa2ea627973bf0a18184
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-05-19 02:33:18 +0300

    feat: profil ekrani eklendi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v7';
+const CACHE_NAME = 'pazar-cache-v8';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
