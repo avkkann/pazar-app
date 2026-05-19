380f7728d7577db41374117c26d4b6cc24ac10b9
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-05-19 03:18:50 +0300

    fix: firsat sepete ekle butonu - id base64 encode + sw v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v9';
+const CACHE_NAME = 'pazar-cache-v10';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
