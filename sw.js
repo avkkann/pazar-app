caa45ab3af5eb97e434502ebd129d08ef6fd65c3
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-05-21 01:47:19 +0300

    fix: openDetay'da urun productMap'e kaydedilir (toggleSepet kategori detayindan eklemiyordu), cache v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v24';
+const CACHE_NAME = 'pazar-cache-v25';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
