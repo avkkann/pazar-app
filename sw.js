9f08e2875adb509c3fce52fe9803e83c114962ad
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-06 15:19:49 +0300

    feat: marketfiyati.org.tr canli arama POC v1 (v46)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v45';
+const CACHE_NAME = 'pazar-cache-v46';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
