52a9f9dadb74928e5fa2871ae65069d20534c36c
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-15 19:56:32 +0300

    feat: tab ikon pulse efekti + sw v55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v54';
+const CACHE_NAME = 'pazar-cache-v55';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
