b9cc5087150e319a5d06c180ae157db2aaee1b30
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-16 22:47:01 +0300

    perf: digerPaketleri ve rakipMarkalari index'lendi, tuzak seridi chunked

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v62';
+const CACHE_NAME = 'pazar-cache-v63';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
