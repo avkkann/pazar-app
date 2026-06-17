da39f83e931153676cdaa18f8950dfdbfcc39922
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-17 18:39:33 +0300

    feat: kategori kartlarinda PNG fotograf destegi (emoji fallback)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v69';
+const CACHE_NAME = 'pazar-cache-v70';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
