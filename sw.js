a6733e379fa0ee63f057a6056d5a11dff08acbf0
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-21 17:30:26 +0300

    dalga3_adim2: skeleton shimmer dark mode + empty-state redesign (2 renderUrunler varyantı, Lucide ikonlar); SW v93

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v92';
+const CACHE_NAME = 'pazar-cache-v93';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
