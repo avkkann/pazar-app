60706f19ebf78b8458825bc10234b64bf8e13388
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-21 21:38:54 +0300

    hotfix5: profil ikonlar DOMContentLoaded ile inject (data-lc attribute, JS replace); SW v101

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v100';
+const CACHE_NAME = 'pazar-cache-v101';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
