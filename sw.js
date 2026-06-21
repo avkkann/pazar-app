9862c46ca73151fc3c97445a710050d34a649aa2
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-21 21:25:48 +0300

    ui: profil ekrani lucide ikonlar (emoji yerine); SW v100

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v99';
+const CACHE_NAME = 'pazar-cache-v100';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
