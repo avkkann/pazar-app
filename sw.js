e9763f6d602bce28ee3090ea8080ad28a5b9045d
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-06-28 20:10:08 +0300

    feat: fiyat geçmişi P75/P25 > 2.5 olan ürünleri gizle (scraper veri kalitesi)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v109';
+const CACHE_NAME = 'pazar-cache-v110';
 const DATA_URLS = [
   new URL('./data/urunler.json', self.location).href,
   new URL('./data/hal.json', self.location).href,
