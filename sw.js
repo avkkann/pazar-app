1001b440e08b894f580eafe93c88a4df8a67835d
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-07-08 02:13:53 +0300

    feat: GoatCounter analytics eklendi (kartsiz, cerezsiz, KVKK-uyumlu)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v133';
+const CACHE_NAME = 'pazar-cache-v134';
 const DATA_URLS = [
   new URL('./data/hal.json', self.location).href,
   new URL('./data/marketfiyati.json', self.location).href,
