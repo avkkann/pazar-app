ae28a6e5c03807fe98399b23ed0b0bcf24b6c7ec
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-07-08 02:58:23 +0300

    feat: onboarding gorsel olarak profesyonellestirildi (gradient, blob ikon, giris animasyonu, pill dot)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v138';
+const CACHE_NAME = 'pazar-cache-v139';
 const DATA_URLS = [
   new URL('./data/hal.json', self.location).href,
   new URL('./data/marketfiyati.json', self.location).href,
