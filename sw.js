fa4389d36e095c7194f035065135f9ef5122d9a6
Mustafa Karabıyık <mustafaavkan72@gmail.com>
2026-07-08 16:30:34 +0300

    fix: PWA kurulum banner'i konumu duzeltildi, 30sn/ilk-urun tetikleyicisi, 14 gun TTL (P1-U3)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pazar-cache-v139';
+const CACHE_NAME = 'pazar-cache-v140';
 const DATA_URLS = [
   new URL('./data/hal.json', self.location).href,
   new URL('./data/marketfiyati.json', self.location).href,
